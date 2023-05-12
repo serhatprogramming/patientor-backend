8a30a8908aeedefc69410f85706d3fc71b7c73e0
serhatprogramming <serhatprogramming@gmail.com>
2023-05-12 10:35:43 -0400

    cors added to backend

--- build/index.js
+++ build/index.js
@@ -7,9 +7,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const cors_1 = __importDefault(require("cors"));
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.get("/api/ping", (_req, res) => {
     res.send("pong");
 });
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 const app = express();
+import cors from "cors";
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
 });
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
