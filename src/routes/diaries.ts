7fffe687485c21b7c18c58d28fd9d856c6a04d6e
serhatprogramming <serhatprogramming@gmail.com>
2023-05-12 11:30:47 -0400

    data added and util methods to reach the data

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
+import { getEntries } from "../services/diaryService";
 
 const router = express.Router();
 
 router.get("/", (_req, res) => {
-  res.send("fetching all diaries");
+  const diaries = getEntries();
+  console.log("diaries: ", diaries);
+  res.send(getEntries());
 });
 
 router.post("/", (_req, res) => {
---
+++ src/services/diaryService.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import diaryData from "../../data/entries.json";
+
+const getEntries = () => {
+  return diaryData;
+};
+
+const addDiary = () => {
+  return null;
+};
+
+export { getEntries, addDiary };
