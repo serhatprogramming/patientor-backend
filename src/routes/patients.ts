07327f8ca1cbe5f84431b4d709ecf8dc8ef902e1
serhatprogramming <serhatprogramming@gmail.com>
2023-05-13 13:19:11 -0400

    patient service added.

--- src/routes/patients.ts
+++ src/routes/patients.ts
@@ -2,9 +2,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 
+import patientService from "../services/patientService";
+
 const router = express.Router();
 
 router.get("/", (_req, res) => {
+  console.log(patientService.getPatients());
   res.send("patient data...");
 });
 
